cc19da484b083090252993e90d79a1cb692336e3
Muvaffak Onus <me@muvaf.com>
2024-05-02 09:13:49 +0300

    update with kubebuilder v3.14.2
    Signed-off-by: Muvaffak Onus <me@muvaf.com>

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 Muvaffak Onus.
+Copyright 2024 Muvaffak Onus.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the mygroup v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=mygroup.muvaf.github.io
+// +kubebuilder:object:generate=true
+// +groupName=mygroup.muvaf.github.io
 package v1alpha1
 
 import (
--- internal/controller/otherexamplekind_controller.go
+++ internal/controller/otherexamplekind_controller.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 Muvaffak Onus.
+Copyright 2024 Muvaffak Onus.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package controllers
+package controller
 
 import (
 	"context"
@@ -27,36 +27,36 @@ import (
 	mygroupv1alpha1 "github.com/muvaf/kubebuilder-init/api/v1alpha1"
 )
 
-// ExampleKindReconciler reconciles a ExampleKind object
-type ExampleKindReconciler struct {
+// OtherExampleKindReconciler reconciles a OtherExampleKind object
+type OtherExampleKindReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=mygroup.muvaf.github.io,resources=examplekinds,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=mygroup.muvaf.github.io,resources=examplekinds/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=mygroup.muvaf.github.io,resources=examplekinds/finalizers,verbs=update
+//+kubebuilder:rbac:groups=mygroup.muvaf.github.io,resources=otherexamplekinds,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=mygroup.muvaf.github.io,resources=otherexamplekinds/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=mygroup.muvaf.github.io,resources=otherexamplekinds/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
-// the ExampleKind object against the actual cluster state, and then
+// the OtherExampleKind object against the actual cluster state, and then
 // perform operations to make the cluster state reflect the state specified by
 // the user.
 //
 // For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
-func (r *ExampleKindReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.3/pkg/reconcile
+func (r *OtherExampleKindReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
-	// your logic here
+	// TODO(user): your logic here
 
 	return ctrl.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
-func (r *ExampleKindReconciler) SetupWithManager(mgr ctrl.Manager) error {
+func (r *OtherExampleKindReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&mygroupv1alpha1.ExampleKind{}).
+		For(&mygroupv1alpha1.OtherExampleKind{}).
 		Complete(r)
 }
